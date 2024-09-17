7ffafa2ac812ca9adf768e504529a068dfc8f993
Gabriela B Carvalho <107872122+GGabi40@users.noreply.github.com>
2024-09-17 13:29:27 +0000

    rewrite explanation

--- 1_primerosComandos/2_tiposDatos.py
+++ 1_primerosComandos/2_tiposDatos.py
@@ -1,25 +1,33 @@
-n1 = int(input('Ingrese un numero: '));
-n2 = int(input('Ingrese otro numero: '));
+# Solicitando al usuario que ingrese dos números enteros
+n1 = int(input('Ingrese un número: '))  # El int() convierte el valor ingresado (que es un string) a un número entero
+n2 = int(input('Ingrese otro número: '))  # Igual que en n1, el valor se convierte a entero
 
-otroNumero =  input("Ingrese un tercer numero: ");
-print(' el tercer numero es: ', type(otroNumero));
+# Solicitando un tercer número pero sin convertirlo a entero
+otroNumero = input("Ingrese un tercer número: ")  # Este valor se ingresa como texto (string)
+print('El tercer número es de tipo: ', type(otroNumero))  # Aquí verificamos el tipo de dato del tercer número con type()
 
-resultado = n1 + n2;
-print(n1.is_integer());
+# Sumando los dos primeros números
+resultado = n1 + n2  # Se suman los dos números enteros
+print(n1.is_integer())  # Aquí se verifica si n1 es un número entero utilizando el método is_integer()
 
-print('La suma entre {} y {} es = {}'.format(n1, n2, resultado));
-print(f'La suma entre {n1} y {n2} es = {resultado}');
+# Imprimiendo el resultado de la suma de dos maneras distintas
+print('La suma entre {} y {} es = {}'.format(n1, n2, resultado))  # Se usa el método format() para insertar los valores
+print(f'La suma entre {n1} y {n2} es = {resultado}')  # Aquí se utiliza f-string, que permite insertar variables directamente en el string
 
-#################
+# -----
 
-print("\n");
-n = float(input('Ingrese un valor: '));
-print(n);
+print("\n")  # Salto de línea
 
-#################
+# Solicitando al usuario un valor decimal (flotante)
+n = float(input('Ingrese un valor: '))  # El float() convierte el valor ingresado a un número decimal (flotante)
+print(n)  # Se imprime el número decimal ingresado
 
-print("\n");
-letra = input('Ingrese algo: ');
-print(letra.isupper()); # Pregunta si está en mayuscula
-print(letra.islower()); # Pregunta si está en minuscula
-print(letra.isnumeric());# Pregunta si es un valor numérico
+# -----
+
+print("\n")
+
+# Solicitando al usuario que ingrese un texto
+letra = input('Ingrese algo: ')  # Se recibe una cadena de texto como entrada
+print(letra.isupper())  # Verifica si el texto está en mayúsculas
+print(letra.islower())  # Verifica si el texto está en minúsculas
+print(letra.isnumeric())  # Verifica si el texto ingresado es un valor numérico (solo dígitos)
