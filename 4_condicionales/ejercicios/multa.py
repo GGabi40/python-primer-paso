dca51678b9e8f667f6da31faf2242d913a505665
Gabriela B Carvalho <107872122+GGabi40@users.noreply.github.com>
2024-09-22 22:02:22 +0000

    colores added

--- 4_condicionales/ejercicios/multa.py
+++ 4_condicionales/ejercicios/multa.py
@@ -1,12 +1,18 @@
 from random import randrange;
 from time import sleep;
 
+colores = {
+    'limpia': '\033[m',
+    'amarillo': '\033[33m',
+    'backRojo': '\033[1;41m'
+};
+
 velocidad = randrange(30, 140);
 limite = 80
 multa = (velocidad - limite) * 7;
 
 sleep(2);
-print('\n¡ALTO! Control policial.');
+print(f'\n{colores["backRojo"]}¡ALTO! Control policial. {colores["limpia"]}');
 print('\nLeyendo velocidad del vehículo...\n');
 
 for i in range(5):
@@ -16,7 +22,7 @@ for i in range(5):
 print('\n');
 
 if (velocidad >= limite):
-    print(f'Ibas a {velocidad} km/h. Tenés una multa por exceso de velocidad su costo será de $ {multa:.2f}');
+    print(f'Ibas a {colores['backRojo']}{velocidad} km/h{colores["limpia"]}. Tenés una multa por exceso de velocidad su costo será de {colores['amarillo']}$ {multa:.2f}{colores['limpia']}');
     print('ATRAPADO.');
 else:
-    print(f'Ibas a {velocidad} km/h. Tu velocidad está bien, siga camino.');
+    print(f'Ibas a {colores['amarillo']}{velocidad} km/h{colores["limpia"]}. Tu velocidad está bien, siga camino.');
