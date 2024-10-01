50136e1df9ce945ffc3a3337a671f494310b1c3f
ggabi40 <gabibc2000@hotmail.com>
2024-09-30 23:32:31 -0300

    bucle WHILE

---
+++ 6_bucles/desafios/2_desafioWHILE.py
@@ -0,0 +1,114 @@
+# DESAFÍOS BUCLE WHILE
+# ------------------ #
+
+# EJERCICIO 1
+
+# Crea un programa que lea el sexo 
+# de una persona pero solo acepte 
+# respuestas 'M' o 'F'. Si escribe mal, 
+# pedile que escriba nuevamente el 
+# valor correcto.
+
+# SOLUCIÓN
+
+
+
+# EJERCICIO 2
+
+# Mejore el juego en el
+# cuál la computadora "piensa" en 
+# un número entre 0 a 10. Sólo que 
+# ahora el jugador va a intentar 
+# adivinar hasta lograrlo, mostrando al 
+# final cuántas veces intentó vencer.
+
+# SOLUCIÓN
+
+
+
+# EJERCICIO 3
+
+# Crear un programa que lea dos valores y
+# muestre un menú en pantalla:
+# [1] Sumar
+# [2] Multiplicar
+# [3] Mayor
+# [4] Nuevos números
+# [5] Salir del programa
+# El programa debe realizar la 
+# operación solicitada en cada caso
+
+# SOLUCIÓN
+
+
+
+# EJERCICIO 4
+
+# Crear un programa que lea un numero
+# y haga su factorial.
+# Ejemplo: 5! = 5x4x3x2x1 = 120
+
+# SOLUCIÓN
+
+
+
+# EJERCICIO 5
+
+# Rehacer el ejercicio de la Progresión Aritmético
+# leyendo el término y la razón de una PA, 
+# mostrando los 10 primeros términos de la 
+# progresión utilizando la estructura WHILE
+
+# SOLUCIÓN
+
+
+
+# EJERCICIO 6
+
+# Mejorar el desafío anterior, preguntandole 
+# al usuario si quiere mostrar más términos.
+# El programa encierra cuando él diga que 
+# quiere mostrar Los términos.
+
+# SOLUCIÓN
+
+
+
+# EJERCICIO 7
+
+# Escribir un programa que lea un número 
+# n entero y muestre en pantalla los 
+# n primeros elementos de una 
+# Secuencia de Fibonacci.
+# Ejemplo: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
+
+# SOLUCIÓN
+
+
+
+# EJERCICIO 8
+
+# Escribir un programa que lea vários números 
+# enteros por teclado. El programa solo 
+# dejará cuándo el usuario escriba el valor 999, 
+# que es la condición de parada. Al final, 
+# muestre cuántos números fueron ingresados 
+# y cuál fue la suma entre ellas 
+# (desconsiderar el 999)
+
+# SOLUCIÓN
+
+
+
+# EJERCICIO 9
+
+# Escribir un programa que lea vários números 
+# enteros por teclado. Al final de su ejecución, 
+# mostrar la medica entre todos los valores 
+# y cuál fue el mayor y el menor valores leídos.
+# El programa debe preguntar al usuario 
+# si quiere o no seguir escribiendo numeros.
+# (desconsiderar el 999)
+
+# SOLUCIÓN
+
